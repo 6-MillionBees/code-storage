d2d34f06956e59913d71313e8a97b73783a040f9
6MillionBees <BoettcherAr321@northwested.org>
2024-10-09 12:22:29 -0400

    Add files via upload

--- personal work/print-replace.py
+++ personal work/print-replace.py
@@ -1,3 +1,28 @@
 # Arden Boettcher
-# 10/5/24
-# Print Replace Practice
+# 10/8/24
+# print practice
+
+import time
+
+input('\nStart? ')
+
+def progress(current, total, bar_length = 150):
+    fraction = current / total
+    arrow = int(fraction * bar_length - 1) * '█' + '▒'
+    idk = int(fraction * bar_length) * '█'
+    padding = int(bar_length - len(arrow)) * '-'
+    if fraction >= .99:
+        print(f'''\n\033[A\033[A\rLoading...
+{idk}{padding} {round(current)}%''', end= '')
+    else:
+        print(f'''\n\033[A\033[A\rLoading...
+{arrow}{padding} {round(current)}%''', end= '')
+
+total = 100
+percent = 0.0
+print('\n')
+while percent <= total:
+    progress(percent, total)
+    percent += 1
+    time.sleep(0.1)
+print('\n\nLoading Complete!\n')
