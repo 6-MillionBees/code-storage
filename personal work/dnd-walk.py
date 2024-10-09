d8cfa066d41eff6cb9c576583f920d8fc4cf87b0
6MillionBees <BoettcherAr321@northwested.org>
2024-10-09 15:43:40 -0400

    Add files via upload

---
+++ personal work/dnd-walk.py
@@ -0,0 +1,39 @@
+# Arden Boettcher
+# Start: 10/9/24
+# A short hike in DND
+
+import random
+
+
+def d100():
+    random.randint(1, 100)
+def d20():
+    random.randint(1, 20)
+def d12():
+    random.randint(1, 12)
+def d10():
+    random.randint(1, 10)
+def d8():
+    random.randint(1, 8)
+def d6():
+    random.randint(1, 6)
+def d4():
+    random.randint(1, 4)
+
+while 
+
+distance = input('how long do you want to hike?\n')
+pack = 
+distance_traveled = 0
+
+while distance > distance_traveled:
+    if d20() >= 10:
+        is_incounter = True
+    else:
+        is_incounter = False
+    if d20() >= 10:
+        is_chance = True
+    else:
+        is_chance = False
+
+    distance_traveled += 1
