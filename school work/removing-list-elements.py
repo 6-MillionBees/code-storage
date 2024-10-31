c34d6a2652ff9950c08e7c10816c3fc5d63ae213
6-MillionBees <BoettcherAr321@northwested.org>
2024-10-31 11:19:31 -0400

    modified:   school work/removing-list-elements.py

--- school work/removing-list-elements.py
+++ school work/removing-list-elements.py
@@ -2,6 +2,8 @@
 # 10/30/24
 # Removing List Elements
 
+import time
+
 # Favorites
 
 favorites = ['team fortress 2', 'terraria', 'github', 'will wood', 'finland', 'bbq chicken']
@@ -26,6 +28,8 @@ print(favorites)
 del favorites[2] # Removes finland
 print(favorites)
 
+print()
+print()
 
 
 
@@ -33,15 +37,24 @@ print(favorites)
 
 # Guest list
 
-guest_list = ['Abraham Lincoln', 'Leonardo Da Vinci', 'Pluto', 'Jesus Christ', 'Tony Hawk', 'Gautama Buddha']
-# I don't want to eat dinner with anyone so I'm choosing random historical figures
+guest_list = ['Abraham (The Prophet)', 'Leonardo Da Vinci', 'Laozi', 'Jesus Christ', 'Tony Hawk', 'Gautama Buddha']
+# I don't want to eat dinner with anyone in particular so I'm choosing historical figures that would be the best to bring into our time
+# I get that tony hawk is still alive but It'd be so funny
+# A lot of these are founders of religions
+
 
 print('Keeping the exsistance of God a mystery Jesus failed to arrive.')
 guest_list.remove('Jesus Christ')
+input('Press enter to continue:\n')
 
 for guest in guest_list:
     print(f'''
-Congratulations {guest}
-    You\'ve been invited to the inter-time
-dinner party. Please power on your time machines
-and join us for dinner''')
+Congratulations {guest}!
+    You\'ve been officially invited to the
+inter-time dinner party. Please power on your
+time machines and join us for dinner.
+
+ps. You can sleep in you\'ll always be on time
+''')
+    time.sleep(1)
+
