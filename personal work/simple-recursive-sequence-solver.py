31dbef65ac3ab56b979793245b2dd81589b129dc
6-MillionBees <BoettcherAr321@northwested.org>
2024-11-08 10:44:36 -0500

    modified:   personal work/simple-recursive-sequence-solver.py 	modified:   school work/for-loops-starter-p2.py

--- personal work/simple-recursive-sequence-solver.py
+++ personal work/simple-recursive-sequence-solver.py
@@ -27,7 +27,7 @@ if num3c1 != 0 and num3c2 != 0: # This prevents dividing by 0 related problems
     num3c3 = tuple1 / tuple0
     num3c4 = tuple2 / tuple1
     dif1 = num3c2 / num3c1
-    dif2 = dif1*tuple0 - tuple1
+    dif2 = dif1 * tuple0 - tuple1
     rounded_dif1 = float(f'{dif1:.4f}') # I got shifted geometric to be working
     rounded_dif2 = float(f'{dif2:.4f}') # I'm so fucking smart
     is_shifted_plus = rounded_dif1 * tuple0 + rounded_dif2 == tuple1
