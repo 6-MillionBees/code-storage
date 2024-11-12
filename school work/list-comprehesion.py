0a0b96ada9cf050eeb53fa0b4ca0ce72bf5e9eb4
6-MillionBees <BoettcherAr321@northwested.org>
2024-11-12 12:15:00 -0500

    modified:   .vscode/settings.json 	modified:   school work/list-comprehesion.py

--- school work/list-comprehesion.py
+++ school work/list-comprehesion.py
@@ -6,9 +6,21 @@
 
 # Title Case Names
 
-students = ['georg', 'joshua', 'mikey', 'lily', 'philomina', 'benjamin']
+students = ['GEORG', 'JOSHUA', 'MIKEY', 'LILY', 'PHILOMINA', 'BENJAMIN']
 students_title = [student.title() for student in students]
 
 
 # Filtered Lowercase Names
 
+students_lower = [student.lower() for student in students]
+
+print(students_lower)
+
+
+# Inches To Centemeters
+
+inches = [14, 20, 36, 40]
+centimeters = [inch * 2.54 for inch in inches]
+
+print(f'inches: {inches}')
+print(f'centimeters: {centimeters}')
