fa74ae39206bd1991f784eddc55d3dc2253196d9
6-MillionBees <BoettcherAr321@northwested.org>
2024-11-12 12:21:39 -0500

    modified:   school work/list-comprehesion.py

--- school work/list-comprehesion.py
+++ school work/list-comprehesion.py
@@ -6,13 +6,15 @@
 
 # Title Case Names
 
-students = ['GEORG', 'JOSHUA', 'MIKEY', 'LILY', 'PHILOMINA', 'BENJAMIN']
-students_title = [student.title() for student in students]
+students_upper = ['GEORG', 'JOSHUA', 'MIKEY', 'LILY', 'PHILOMINA', 'BENJAMIN']
+students_title = [student.title() for student in students_upper]
+print(students_upper)
+print(students_title)
 
 
 # Filtered Lowercase Names
 
-students_lower = [student.lower() for student in students]
+students_lower = [student.lower() for student in students_upper]
 
 print(students_lower)
 
@@ -24,3 +26,12 @@ centimeters = [inch * 2.54 for inch in inches]
 
 print(f'inches: {inches}')
 print(f'centimeters: {centimeters}')
+
+
+# filtered last names
+
+last_names = ['Williams', 'Perez', 'Matthes', 'Brown', 'Carlsen', 'Brimley', 'Vincent']
+
+bast_bames = [name for name in last_names if name.lower().startswith('b')]
+
+print(bast_bames)
