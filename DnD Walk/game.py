b01deebd2041e70af5d4a6f8c3ea4c31e6f021e7
6-MillionBees <BoettcherAr321@northwested.org>
2024-11-18 11:17:44 -0500

    modified:   .vscode/settings.json 	modified:   DnD Walk/game.py 	new file:   school work/parameter-defaults-main.py

--- DnD Walk/game.py
+++ DnD Walk/game.py
@@ -14,35 +14,147 @@ Y = 400
 width = 50
 height = 50
 screen = pygame.display.set_mode((X, Y))
-rect = Rect(200, 80, width, height)
+
+rect1 = Rect(300, 80, width, height)
+rect2 = Rect(200, 80, width, height)
+rect3 = Rect(100, 80, width, height)
+
+rect1_hit = False
+rect2_hit = False
+rect3_hit = False
+
+
+menu_basic_triple = Rect(100, 100, 200, 50)
+menu_triple_combo = Rect(100, 200, 200, 50)
+
 running = True
 score = 0
+
 font = pygame.font.Font('CascadiaCodeNF.ttf', 15)
-text1 = font.render(f'click', True, (255, 255, 255), None)
-text2 = font.render(f'me!', True, (255, 255, 255), None)
+text1_rect1 = font.render(f'click', True, (255, 255, 255), None)
+text2_rect1 = font.render(f'me!', True, (255, 255, 255), None)
+text1_rect2 = font.render(f'click', True, (255, 255, 255), None)
+text2_rect2 = font.render(f'me!', True, (255, 255, 255), None)
+text1_rect3 = font.render(f'click', True, (255, 255, 255), None)
+text2_rect3 = font.render(f'me!', True, (255, 255, 255), None)
+
+gamemode = None
+
+red = (255, 0, 0)
 
 while running:
+
+    if gamemode == 'triple combo':
+        if rect1_hit and rect2_hit and rect3_hit:
+            rect1 = Rect(randint(0, X - width), randint(0, Y - height), width, height)
+            rect2 = Rect(randint(0, X - width), randint(0, Y - height), width, height)
+            rect3 = Rect(randint(0, X - width), randint(0, Y - height), width, height)
+
+            rect1_hit = False
+            rect2_hit = False
+            rect3_hit = False
+
+        for event in pygame.event.get():
+
+            if event.type == QUIT:
+                print(score)
+                running = False
+
+            elif event.type == MOUSEBUTTONDOWN:
+                if rect1.collidepoint(event.pos):
+                    score += 1
+                    rect1[0] = 1000
+                    rect1_hit = True
+
+                elif rect2.collidepoint(event.pos):
+                    score += 1
+                    rect2[0] = 1000
+                    rect2_hit = True
+
+                elif rect3.collidepoint(event.pos):
+                    score += 1
+                    rect3[0] = 1000
+                    rect3_hit = True
+
+        screen.fill((127,127,127))
+
+        pygame.draw.rect(screen, red, rect1)
+        screen.blit(text1_rect1, (rect1[0] + 3, rect1[1] + 9))
+        screen.blit(text2_rect1, (rect1[0] + 14, rect1[1] + 23))
+
+        pygame.draw.rect(screen, red, rect2)
+        screen.blit(text1_rect2, (rect2[0] + 3, rect2[1] + 9))
+        screen.blit(text2_rect2, (rect2[0] + 14, rect2[1] + 23))
+
+        pygame.draw.rect(screen, red, rect3)
+        screen.blit(text1_rect3, (rect3[0] + 3, rect3[1] + 9))
+        screen.blit(text2_rect3, (rect3[0] + 14, rect3[1] + 23))
+
+        pygame.display.set_caption(f'score: {score}')
+        pygame.display.flip()
+        continue
+
+
+    elif gamemode == 'basic triple':
+        for event in pygame.event.get():
+
+            if event.type == QUIT:
+                print(score)
+                running = False
+                
+
+            elif event.type == MOUSEBUTTONDOWN:
+                if rect1.collidepoint(event.pos):
+                    score += 1
+                    rect1 = Rect(randint(0, X - width), randint(0, Y - height), width, height)
+                elif rect2.collidepoint(event.pos):
+                    score += 1
+                    rect2 = Rect(randint(0, X - width), randint(0, Y - height), width, height)
+                elif rect3.collidepoint(event.pos):
+                    score += 1
+                    rect3 = Rect(randint(0, X - width), randint(0, Y - height), width, height)
+
+            # elif event.type == MOUSEMOTION:
+            #     mouse_pos = pygame.mouse.get_pos()
+            #     print(f'({mouse_pos[0]}, {mouse_pos[1]})')
+            #     rect = Rect(mouse_pos[0], mouse_pos[1], 50, 60)
+
+        screen.fill((127,127,127))
+
+        pygame.draw.rect(screen, red, rect1)
+        screen.blit(text1_rect1, (rect1[0] + 3, rect1[1] + 9))
+        screen.blit(text2_rect1, (rect1[0] + 14, rect1[1] + 23))
+
+        pygame.draw.rect(screen, red, rect2)
+        screen.blit(text1_rect2, (rect2[0] + 3, rect2[1] + 9))
+        screen.blit(text2_rect2, (rect2[0] + 14, rect2[1] + 23))
+
+        pygame.draw.rect(screen, red, rect3)
+        screen.blit(text1_rect3, (rect3[0] + 3, rect3[1] + 9))
+        screen.blit(text2_rect3, (rect3[0] + 14, rect3[1] + 23))
+
+        pygame.display.set_caption(f'score: {score}')
+        pygame.display.flip()
+        continue
+
     for event in pygame.event.get():
 
         if event.type == QUIT:
-            print(score)
             running = False
+            continue
 
         elif event.type == MOUSEBUTTONDOWN:
-            if rect.collidepoint(event.pos):
-                score += 1
-                rect = Rect(randint(width, X - width), randint(height, Y - height), width, height)
+            if menu_basic_triple.collidepoint(event.pos):
+                gamemode = 'basic triple'
+            elif menu_triple_combo.collidepoint(event.pos):
+                gamemode = 'triple combo'
 
-        # elif event.type == MOUSEMOTION:
-        #     mouse_pos = pygame.mouse.get_pos()
-        #     print(f'({mouse_pos[0]}, {mouse_pos[1]})')
-        #     rect = Rect(mouse_pos[0], mouse_pos[1], 50, 60)
+
 
     screen.fill((127,127,127))
-    pygame.draw.rect(screen, (255,0,0), rect)
-    screen.blit(text1, (rect[0] + 3, rect[1] + 9))
-    screen.blit(text2, (rect[0] + 14, rect[1] + 23))
-    pygame.display.set_caption(f'score: {score}')
+    pygame.draw.rect(screen, red, menu_basic_triple)
+    pygame.draw.rect(screen, red, menu_triple_combo)
     pygame.display.flip()
+
 
 pygame.quit()
