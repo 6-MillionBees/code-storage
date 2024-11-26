aec94d78d7308dd8e5b98b122cbb5f743afaed42
6-MillionBees <Boettcherar321@northwested.org>
2024-11-26 13:06:35 -0500

    new file:   personal work/coup/__pycache__/colors.cpython-312.pyc 	new file:   personal work/coup/colors.py 	modified:   personal work/coup/main.py

---
+++ personal work/coup/colors.py
@@ -0,0 +1,17 @@
+white = [255, 255, 255]
+black = [0, 0, 0]
+
+duke_purple = [170, 49, 245]
+duke_gold = [255, 172, 56]
+
+contessa_red = [227, 30, 16]
+contessa_orange = [227, 82, 20]
+
+captain_blue = [0, 58, 110]
+captain_light_blue = [54, 134, 207]
+
+assasin_grey = [19, 28, 36]
+assasin_blue = [27, 63, 94]
+
+ambassador_green = [83, 163, 3]
+ambassador_orange = [255, 119, 0]
--- personal work/coup/main.py
+++ personal work/coup/main.py
@@ -4,25 +4,25 @@
 
 import pygame
 from pygame.locals import *
+from colors import *
 pygame.init()
 
-
-window = pygame.display.set_mode([500, 500])
-pygame.display.set_caption('Coup')
+version = "0.1.0"
+screen = pygame.display.set_mode([500, 500])
+pygame.display.set_caption('Coup ', version)
 
 running = True
 rect = Rect(200, 200, 50, 50)
 number = 0
-white = [255, 255, 255]
-black = [0, 0, 0]
+
 
 def outline(rect, color = black):
     outline_rect = rect
     outline_rect = Rect(outline_rect[0], outline_rect[1], outline_rect[2] + 2, outline_rect[3] + 2)
     outline_rect.center = rect.center
-    pygame.draw.rect(window, color, outline_rect)
+    pygame.draw.rect(screen, color, outline_rect)
 
-def render_text(words, pos_x, pos_y, size = 50, color = black, rect_color = white):
+def render_text(words, pos_x, pos_y, size, color = black, rect_color = white):
     font = pygame.font.Font('CascadiaCodeNF.ttf', size)
     text = font.render(str(words), True, color, rect_color)
     text_rect = text.get_rect()
@@ -30,11 +30,11 @@ def render_text(words, pos_x, pos_y, size = 50, color = black, rect_color = whit
     return text, text_rect
 
 while running:
-    text1 = render_text(str(number), 250, 250)
 
     for event in pygame.event.get():
 
         if event.type == QUIT:
+
             running = False
             continue
 
@@ -42,10 +42,10 @@ while running:
             if text1[1].collidepoint(event.pos):
                 number += 1
 
-    window.fill(white)
-    # pygame.draw.rect(window, white, rect)
+    screen.fill(white)
+    text1 = render_text(str(number), 250, 250, 50)
     outline(text1[1])
-    window.blit(text1[0], text1[1])
+    screen.blit(text1[0], text1[1])
 
 
     pygame.display.flip()
