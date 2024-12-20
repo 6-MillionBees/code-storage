ffd3eae4b1a07a5e32f675c80e6762b27a5a8ed0
6-MillionBees <BoettcherAr321@northwested.org>
2024-12-20 11:17:29 -0500

    modified:   java/website.html 	renamed:    personal work/pink settings.json -> personal work/Extra Settings/pink settings.json 	renamed:    personal work/Gruvbox Settings/settings.json -> personal work/Extra Settings/settings.json 	renamed:    personal work/your-blood-will-boil/SUNP0118.JPG -> personal work/Your Blood Will Boil Demo/SUNP0118.JPG 	renamed:    personal work/your-blood-will-boil/colors.md -> personal work/Your Blood Will Boil Demo/colors.md 	renamed:    personal work/your-blood-will-boil/main.py -> personal work/Your Blood Will Boil Demo/main.py 	renamed:    personal work/your-blood-will-boil/tetris.py -> personal work/Your Blood Will Boil Demo/tetris.py 	new file:   personal work/indent_formatter.py 	renamed:    personal work/a-ghost.py -> personal work/misc/a-ghost.py 	renamed:    personal work/def-practice.py -> personal work/misc/def-practice.py 	renamed:    personal work/drawing-practice.py -> personal work/misc/drawing-practice.py 	renamed:    personal work/game.py -> personal work/misc/game.py 	renamed:    personal work/if-else-practice.py -> personal work/misc/if-else-practice.py 	renamed:    personal work/image-showing.py -> personal work/misc/image-showing.py 	renamed:    personal work/math_thingy.py -> personal work/misc/math_thingy.py 	renamed:    personal work/numbers-practice.py -> personal work/misc/numbers-practice.py 	renamed:    personal work/print-replace.py -> personal work/misc/print-replace.py 	renamed:    personal work/rando.py -> personal work/misc/rando.py 	renamed:    personal work/simple-recursive-sequence-finder.py -> personal work/misc/simple-recursive-sequence-finder.py 	renamed:    personal work/simple-recursive-sequence-solver.py -> personal work/misc/simple-recursive-sequence-solver.py 	renamed:    personal work/string-methods-practice.py -> personal work/misc/string-methods-practice.py

---
+++ personal work/indent_formatter.py
@@ -0,0 +1,54 @@
+# Arden Boettcher
+# 12/20/24
+# Indent Formatter
+
+
+while True:
+
+  file_start_name = input('Please enter file name (0 to quit):\n')
+
+  if file_start_name == '0':
+    print('\nOkie doki bye bye now!\n')
+    quit()
+
+  try:
+    file_start = open(file_start_name, "r")
+  except FileNotFoundError:
+    print('\nFile Not Found: please make sure it is the exact file name.')
+    print('Make sure to include the file format (eg. .txt or .json)\n')
+  else:
+    break
+
+while True:
+  try:
+    starting_whitespace = int(input('How many starting whitespace characters? (-1 to exit)\n'))
+    print()
+    final_whitespace = int(input('How many desired whitespace characters? (-1 to exit)\n'))
+    print()
+  except ValueError:
+    print('Invalid Input: please enter a number')
+  else:
+    break
+
+period_index = file_start_name.index('.')
+file_end = open(file_start_name[0: period_index] + '_formatted' + file_start_name[period_index: ], 'w')
+
+file_start_read = file_start.read()
+current_index = -1
+cooldown = 0
+letter_cooldown = 0
+
+for letter in file_start_read:
+  current_index += 1
+
+  if file_start_read[current_index: current_index + starting_whitespace] == ' ' * starting_whitespace:
+    if cooldown <= 0:
+      file_end.write(' ' * final_whitespace)
+      cooldown = starting_whitespace - 1
+    else:
+      cooldown -= 1
+
+  else:
+    if cooldown <= 0:
+      file_end.write(letter)
+    cooldown -= 1
