0bbe76ec4c81ebbc3bf8800fa41530ee963b2d0c
6-MillionBees <BoettcherAr321@northwested.org>
2025-01-10 11:19:10 -0500

    new file:   school work/passing_values.py

---
+++ school work/passing_values.py
@@ -0,0 +1,16 @@
+# Arden Boettcher
+# 1/10/25
+# Passing Values
+
+def square_number(number):
+  return number ** 2
+
+def add_squares(num1, num2):
+  return num1 + num2
+
+def get_int(words):
+  while True:
+    try:
+      result = input(words)
+
+def main():
